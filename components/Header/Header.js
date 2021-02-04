bc811fd4d81ff4fd09d08be07d1df916ef7ba48c
nely de faria <nelly.nf54@gmail.com>
2021-02-04 19:16:07 -0300

    Adiciona link font awesome

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -7,6 +7,7 @@ export default function Header(props) {
             <Head>
                 <title>{props.title}</title>
                 <link rel="stylesheet" href="/styles/page.css" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
              </Head>
             <Link href="/">
                 <a>
